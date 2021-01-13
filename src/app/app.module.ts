7646ba25786afd24bdbc0f818175ef1081fd92ff
Umutcan Alpak <umutcanalpak@gmail.com>
2021-01-13 23:30:01 +0300

    department-show-dep component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentComponent
+    DepartmentComponent,
+    ShowDepComponent
   ],
   imports: [
     BrowserModule,
