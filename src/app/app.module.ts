8ec03fb20663b9579798a608b60143ffd490ba36
Umutcan Alpak <umutcanalpak@gmail.com>
2021-01-13 23:30:46 +0300

    department-add-edit-dep component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentComponent } from './department/department.component';
 import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { AddEditDepComponent } from './department/add-edit-dep/add-edit-dep.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DepartmentComponent,
-    ShowDepComponent
+    ShowDepComponent,
+    AddEditDepComponent
   ],
   imports: [
     BrowserModule,
