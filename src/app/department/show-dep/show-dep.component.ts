6cc0d74efc6702e1c8c3cdb2e41af14c25420743
Umutcan Alpak <umutcanalpak@gmail.com>
2021-01-17 23:50:32 +0300

    show deparment component updates

--- src/app/department/show-dep/show-dep.component.ts
+++ src/app/department/show-dep/show-dep.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-dep',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowDepComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
 
   ngOnInit(): void {
+    this.refreshDepList;
   }
 
+  DepartmentList: any = [];
+  refreshDepList() {
+    this.service.getDepList().subscribe(data => {
+      this.DepartmentList = data;
+    });
+  }
 }
